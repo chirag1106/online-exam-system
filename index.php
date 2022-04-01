abb9be88b2341249174de8a8cf28de2f0c027c85
chirag gupta <chiragg1106@gmail.com>
2022-04-01 23:47:41 +0530

    css setted

--- index.php
+++ index.php
@@ -183,7 +183,7 @@
   </div>
 
   <div style="padding-top:10px" class="footer Cositas-Rorey-2-4-hex">
-    <center>&copy;<script>document.write(new Date().getFullYear())</script> || Developed and Designed by Chirag Gupta and Vikrant Tanwar.</center>
+    <center>&copy;<script>document.write(new Date().getFullYear())</script><br>Developed and Designed by Chirag Gupta and Vikrant Tanwar.</center>
   </div>
 </body>
 
