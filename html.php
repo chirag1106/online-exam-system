789681533f1e7b93f178b230cdb101f72a27d95f
Tanwar2000 <vikranttanwar2000@gmail.com>
2022-04-01 13:26:20 +0530

    updated

--- html.php
+++ html.php
@@ -3,10 +3,12 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<link href='https://fonts.googleapis.com/css?family=Sacramento:400' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="style.css">
 <title>DGI | TEST YOUR SKILL </title>
- <script src="js/jquery.js" type="text/javascript"></script>
+ <script src="./online-examination-systen-in-php-master-1/js/jquery.js" type="text/javascript"></script>
+ 
+<script src="menu.js" type="text/javascript"></script>
  	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
 <?php if(@$_GET['w'])
 {echo'<script>alert("'.@$_GET['w'].'");</script>';}
@@ -22,15 +24,60 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
 
 <body>
 
-  <nav class="nav Treasurana---Geocaching-App-2-hex">
+  <nav class="nav">
     <div class="logo">
       <img src="https://gnindia.dronacharya.info/images/DGI-Logo.png" alt="Dronacharya Group of Institutions">
     </div>
+    <ul>
+      <li><a href=""><img src="admin.png" alt="Admin"></a></li>
+      <li><a href=""><img src="admin.png" alt="Admin"></a></li>
+      <li><a href=""><img src="admin.png" alt="Admin"></a></li>
+    </ul>
+    <div id="ham" class="ham-menu">
+    <img src="menu.png" alt="">
+    </div>
   </nav>
+  <div class="ham-menu-item">
+  <ul>
+      <li><a href="">Admin</a></li>
+      <li><a href="">Developer</a></li>
+      <li><a href="">Feedback</a></li>
+    </ul>
+  </div>
+  <!-- <div class="trans-bg"></div> -->
   <div class="section">
-  <div class="container Treasurana---Geocaching-App-2-hex">
-  <form action="/action_page.php">
-    <div class="row">
+    <div class="container">
+      <form>
+        <h1><center>Sign In<br><h6>(Students)</h6></center></h1>
+        <div class="row">
+          
+          <div class="col">
+            <input type="text" id="email" name="mail" placeholder="Enter Your Email_Id">
+            </div>
+          </div>
+          <div class="row">
+            
+            <div class="col">
+              <input type="text" id="password" name="password" placeholder="Enter Your Password">
+            </div>
+          </div>
+          <div class="row">
+          <input type="submit" value="Submit">
+        </div>
+      </form>
+    </div>
+    <div class="mid">
+      <!-- <div class="mid-1"></div> -->
+      <!-- <h1>Or</h1> -->
+      <!-- <div class="mid-1"></div> -->
+    </div>
+    <!-- <h1 class="neonText">
+      QUIZ TIME?
+    </h1> -->
+    <div class="container">
+      <form action="/action_page.php">
+        <h1><center>Sign Up</center></h1>
+        <div class="row">
       
       <div class="col">
         <input type="text" id="full_name" name="full_name" placeholder="Your full name..">
@@ -52,12 +99,7 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
         </select>
       </div>
     </div>
-    <div class="row">
-      
-      <div class="col">
-        <input type="text" id="college_name" name="college_name" placeholder="Enter Your College Name">
-      </div>
-    </div>
+    
     <div class="row">
       <div class="col">
         <input type="number" id="Mobile_no" name="Mobile_no" placeholder="Enter Your Mobile Number">
@@ -80,12 +122,13 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
 </div>
   </div>
 
-  <div class="footer Treasurana---Geocaching-App-1-hex">
-    <ul>
+  <div style="padding-top:10px" class="footer Cositas-Rorey-2-4-hex">
+  <center>&copy; <script>document.write(new Date().getFullYear())</script> || Developed and Designed by Chirag Gupta and Vikrant Tanwar.</center>
+    <!-- <ul>
       <li><a href="">ADMIN</a></li>
       <li><a href="">DEVELOPERS</a></li>
       <li><a href="">FEEDBACK</a></li>
-    </ul>
+    </ul> -->
   </div>
 </body>
 </html>
