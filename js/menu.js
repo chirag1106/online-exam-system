b7866563a22ade9475ce502d370b6737ef5ff17d
chirag gupta <chiragg1106@gmail.com>
2022-04-05 20:38:39 +0530

    modified js

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,22 @@
-
-
-$(document).ready(function(){
-$("#ham").on('click',function(){
+$(document).ready(function () {
+  $("#ham").on("click", function () {
     $(".ham-menu-item").show(200);
     $("#ham").hide(1);
     $("#close").show(1);
-});
-$("#close").on('click',function(){
+  });
+  $("#close").on("click", function () {
     $("#close").hide(1);
     $("#ham").show(1);
     $(".ham-menu-item").hide(200);
-});
-$(".admin").on('click',function(){
+  });
+  $(".admin").on("click", function () {
     $("#student").hide(1);
     $("#admin").show(1);
     $(".registration").hide(1);
     $(".mid").hide(1);
-});
+  });
 
-
-
-$(".ham-admin").on('click',function(){
+  $(".ham-admin").on("click", function () {
     $("#ham").show(1);
     $(".registration").hide(1);
     $(".mid").hide(1);
@@ -28,27 +24,23 @@ $(".ham-admin").on('click',function(){
     $(".ham-menu-item").hide(200);
     $("#student").hide(1);
     $("#admin").show(1);
-});
-$(".ham-developer").on('click',function(){
+  });
+  $(".ham-developer").on("click", function () {
     $("#ham").show(1);
     $("#close").hide(1);
     $(".ham-menu-item").hide(200);
-});
-$(".ham-feedback").on('click',function(){
+  });
+  $(".ham-feedback").on("click", function () {
     $("#ham").show(1);
     $("#close").hide(1);
     $(".ham-menu-item").hide(200);
-});
-    
-      
-    
-        $(window).resize(function(e) {
-            if ($(window).width() < 1200) {
-                $(".ham-menu-item").hide(200);
-                $("#close").hide(1);
-                $("#ham").show(1);
-            }
-          });
-    
+  });
 
+  $(window).resize(function (e) {
+    if ($(window).width() < 1200) {
+      $(".ham-menu-item").hide(200);
+      $("#close").hide(1);
+      $("#ham").show(1);
+    }
+  });
 });
