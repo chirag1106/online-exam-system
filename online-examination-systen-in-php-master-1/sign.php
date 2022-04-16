13c621af2a975373868ceb2d05662754152821ff
chirag gupta <chiragg1106@gmail.com>
2022-04-16 15:15:50 +0530

    minor

--- online-examination-systen-in-php-master-1/sign.php
+++ online-examination-systen-in-php-master-1/sign.php
@@ -4,52 +4,51 @@ require_once './helperFuncs.php';
 
 // print_r($_POST);
 // exit();
-// ob_start();
+ob_start();
 // if (isset($_POST['uemail'])) {
-    $name = test_input($_POST['uname']);
-    $name = ucwords(strtolower($name));
-    $gender = test_input($_POST['ugender']);
-    $email = test_input($_POST['uemail']);
-    $college = test_input($_POST['ucollege']);
-    $phoneNo = test_input($_POST['uphoneno']);
-    $phoneNo = (int)$phoneNo;
-    // echo var_dump((int)$phoneNo);
+$name = test_input($_POST['uname']);
+$name = ucwords(strtolower($name));
+$gender = test_input($_POST['ugender']);
+$email = test_input($_POST['uemail']);
+$college = test_input($_POST['ucollege']);
+$phoneNo = test_input($_POST['uphoneno']);
+$phoneNo = (int)$phoneNo;
+// echo var_dump((int)$phoneNo);
+// exit();
+$password = test_input($_POST['upassword']);
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo 'Please enter valid email';
+    exit();
+}
+$loggedUser = getCurrentUserDetails($email);
+// print_r($loggedUser);
+// exit();
+if (!empty($loggedUser)) {
+    echo 'Already registered';
+    exit();
+} else {
+    $password = password_hash($password, PASSWORD_BCRYPT);
+    // $password = md5($password);
+    // echo $password;
     // exit();
-    $password = test_input($_POST['upassword']);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo 'Please enter valid email';
-        exit();
-    }
-    $loggedUser = getCurrentUserDetails($email);
-    // print_r($loggedUser);
+    $stmt = $con->prepare('INSERT INTO user VALUES (?, ?, ?, ?, ?, ?)');
+    $stmt->bind_param('ssssss', $name, $gender, $college, $email, $phoneNo, $password);
+    $stmt->execute();
+    $result = $stmt->affected_rows;
+    // print_r($result);
     // exit();
-    if (!empty($loggedUser)) {
-        echo 'Already registered';
-        exit();
+    if ($result) {
+        session_start();
+        $_SESSION['email'] = $email;
+        $_SESSION['name'] = $name;
+        echo 'Registered Successfully';
+        // header("location:account.php?q=1");
     } else {
-        $password = password_hash($password, PASSWORD_BCRYPT);
-        // $password = md5($password);
-        // echo $password;
-        // exit();
-        $stmt = $con->prepare('INSERT INTO user VALUES (?, ?, ?, ?, ?, ?)');
-        $stmt->bind_param('ssssss', $name, $gender, $college, $email, $phoneNo, $password);
-        $stmt->execute();
-        $result = $stmt->affected_rows;
-        // print_r($result);
-        // exit();
-        if ($result) {
-            session_start();
-            $_SESSION['email'] = $email;
-            $_SESSION['name'] = $name;
-            echo 'Registered Successfully';
-            // header("location:account.php?q=1");
-        } 
-        else {
-            echo 'Some error Occured';
-            // header("location:index.php?q7=Email Already Registered!!!");
-        }
-        // ob_end_flush();
+        echo 'Some error Occured';
+        // header("location:index.php?q7=Email Already Registered!!!");
     }
+}
+ob_end_flush();
 // }
 // else{
 //     header('location:../index.php');
