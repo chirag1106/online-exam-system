90cfdc5f0c1693047fa186aba324c1df2c33d85a
Tanwar2000 <vikranttanwar2000@gmail.com>
2022-04-17 00:37:01 +0530

    UPDATE

--- developer.php
+++ developer.php
@@ -18,20 +18,22 @@
                 <div class="img-out">
                     <img src="./images/developer.png" alt="">
                 </div>
-                <center><h3>Chirag Gupta</h3></center>
-                <div class="social-links">
-                            <button>
-                                <a  href="https://www.github.com/chirag1106"><img  src="./images/github.png" alt="Github"></a>
+                <div class="info">
+                    <div class="empty"></div>
 
-                            </button>
-                            <button>
-                                <img src="./images/instagram.png" alt="Instagram">
-
-                            </button>
-                            <button>
-                                <img src="./images/linkedin.png" alt="LinkedIn">
-
-                            </button>
+                    <center><h3>Chirag Gupta</h3></center>
+                    <center><h6>Full-stack Developer</h6></center>
+                    <center><h6>E-mail: chiraggupta1106@gmail.com</h6></center>
+                    <div class="social-links">
+                        <button>
+                            <a  href="https://www.github.com/chirag1106"><img  src="./images/portfolio.png" alt="Github"></a>
+                            
+                        </button>
+                        <button>
+                            <img src="./images/linkedin.png" alt="LinkedIn">
+                            
+                        </button>
+                    </div>
                 </div>
             </div>
             <!-- <div class="mid red"></div> -->
@@ -40,21 +42,26 @@
                 <div class="img-out">
                     <img src="./images/developer.png" alt="">
                 </div>
-                <center><h3>Vikrant Tanwar</h3></center>
-                <div class="social-links">
-                            <button>
-                                <a  href="https://www.github.com/Tanwar2000"><img  src="./images/github.png" alt="Github"></a>
+                
+                <div class="info">
+                    <div class="empty"></div>
+                    <center><h3>Vikrant Tanwar</h3></center>
+                    <center><h6>Full-stack Developer</h6></center>
+                    <center><h6>E-mail: vikranttanwar2000@gmail.com</h6></center>
 
-                            </button>
-                            <button>
-                                <img src="./images/instagram.png" alt="Instagram">
-
-                            </button>
-                            <button>
-                                <img src="./images/linkedin.png" alt="LinkedIn">
-
-                            </button>
+                    <div class="social-links">
+                                <button>
+                                    <a  href="https://vikrant-portfolio.avcno.com"><img  src="./images/portfolio.png" alt="Github"></a>
+    
+                                </button>
+                                
+                                <button>
+                                    <img src="./images/linkedin.png" alt="LinkedIn">
+    
+                                </button>
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>
