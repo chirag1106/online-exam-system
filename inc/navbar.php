8923851e75d4145d89ef775055eee48b11c3c33f
chirag gupta <chiragg1106@gmail.com>
2022-04-17 16:48:13 +0530

    loading time reduce

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="nav">
     <div class="logo">
-      <img src="https://gnindia.dronacharya.info/images/DGI-Logo.png" alt="Dronacharya Group of Institutions">
+      <img src="https://gnindia.dronacharya.info/images/DGI-Logo.png" alt="Dronacharya Group of Institutions" loading="lazy">
     </div>
     <ul id="navbar">
       <li><a class="home" href="javascript:void(0)"><img src="./images/home.png" alt="Home"></a>
