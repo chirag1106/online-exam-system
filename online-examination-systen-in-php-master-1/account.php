a7c4fd2a09eb2e59136f36704bac2f8ff6166cf7
chirag gupta <chiragg1106@gmail.com>
2022-04-23 16:49:39 +0530

    major formatting in the code for better reading

--- online-examination-systen-in-php-master-1/account.php
+++ online-examination-systen-in-php-master-1/account.php
@@ -76,9 +76,9 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li <?php if (@$_GET['q'] == 1) echo 'class="active"'; ?>><a href="account.php?q=1"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home<span class="sr-only">(current)</span></a></li>
-            <li <?php if (@$_GET['q'] == 2) echo 'class="active"'; ?>><a href="account.php?q=2"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>&nbsp;History</a></li>
-            <li <?php if (@$_GET['q'] == 3) echo 'class="active"'; ?>><a href="account.php?q=3"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;Ranking</a></li>
+            <li <?php if (isset($_GET['q']) && $_GET['q'] == 1) echo 'class="active"'; ?>><a href="account.php?q=1"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home<span class="sr-only">(current)</span></a></li>
+            <li <?php if (isset($_GET['q']) && $_GET['q'] == 2) echo 'class="active"'; ?>><a href="account.php?q=2"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>&nbsp;History</a></li>
+            <li <?php if (isset($_GET['q']) && $_GET['q'] == 3) echo 'class="active"'; ?>><a href="account.php?q=3"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;Ranking</a></li>
             <!-- <li class="pull-right"> <a href="logout.php?q=account.php"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Signout</a></li> -->
           </ul>
           <form class="navbar-form navbar-left" role="search">
--- online-examination-systen-in-php-master-1/dbConnection.php
+++ online-examination-systen-in-php-master-1/dbConnection.php
@@ -1,15 +1,15 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$password = '';
+$password = 'root';
 $dbname = 'project';
-// $con= new mysqli('localhost','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('localhost','root','root','project')or die("Could not connect to mysql".mysqli_error($con));
 
-try {
-    $con = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$password");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo 'Connected successfully';
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
+// try {
+//     $con = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$password");
+//     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     // echo 'Connected successfully';
+// } catch (PDOException $e) {
+//     echo 'Connection failed: ' . $e->getMessage();
+// }
 // require __DIR__ . '/vendor/autoload.php';
